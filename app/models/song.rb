65fd91694f94b2d6495a07717a9129cb8a1ace52
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-16 14:16:53 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   validates :release_year, presence: true, if: :released
   validates :artist_name, presence: true
   validate :release_year_cannot_be_after_current_year
-  
+
   def release_year_cannot_be_after_current_year
     if release_year.present? && release_year > Date.today.year
       errors.add(:release_year, "can't be in the future")
