4703cce17fb482a34d895a60c1a2b013dc63cdd4
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-16 14:16:46 +0000

    automatically backed up by learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,14 +4,7 @@ class Song < ActiveRecord::Base
   validates :release_year, presence: true, if: :released
   validates :artist_name, presence: true
   validate :release_year_cannot_be_after_current_year
-  validate :title_author_name_not_duplicate_in_same_year
-
-  def title_author_name_not_duplicate_in_same_year
-    if title.present?
-     errors.add(:title, "can't be released by same artist in same year")
-    end
-  end
-
+  
   def release_year_cannot_be_after_current_year
     if release_year.present? && release_year > Date.today.year
       errors.add(:release_year, "can't be in the future")
